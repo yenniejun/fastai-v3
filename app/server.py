9fba30812c102372ddac1cfcabad9f7c1ba5fec2
Yennie Jun <yennie.jun@gmail.com>
2019-09-23 17:32:47 -0500

    New export file

--- app/server.py
+++ app/server.py
@@ -9,7 +9,8 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://drive.google.com/uc?export=download&id=1Oo2mzJpNxV-VS5jlq2g4zR18UdGnrWA_'
+export_file_url = 'https://drive.google.com/uc?export=download&id=1LzJOf1t89KpLgr3bbvvR4vLW0ZtLiBkq'
+#export_file_url = 'https://drive.google.com/uc?export=download&id=1Oo2mzJpNxV-VS5jlq2g4zR18UdGnrWA_'
 #export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
 export_file_name = 'export.pkl'
 
